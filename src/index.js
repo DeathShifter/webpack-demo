1db979b237d0159e426d8609d22ca174bdac2a5c
Will Linux <druids50@gmail.com>
2022-02-01 12:56:14 -0600

    ffff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import myName from './myName';
+import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
 
-function component() {
+ function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.innerHTML = myName('Cody');
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
-}
+ }
 
-document.body.appendChild(component());
+ document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
